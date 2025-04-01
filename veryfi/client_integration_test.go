283b915fb574601d2e025ca3dbb6515b02097a91
Matthew Eng <matengm1@gmail.com>
2025-04-01 10:14:28 -0700

    Fix integration test

--- veryfi/client_integration_test.go
+++ veryfi/client_integration_test.go
@@ -139,9 +139,9 @@ func TestIntegrationSuccessProcessDocumentUpload(t *testing.T) {
 	assert.NoError(t, err)
 
 	resp, err := client.ProcessDocumentUpload(scheme.DocumentUploadOptions{
-		FilePath: fmt.Sprintf("%s/testdata/receipt_public.jpeg", testpath),
+		FilePath: fmt.Sprintf("%s/testdata/receipt_public.jpg", testpath),
 		DocumentSharedOptions: scheme.DocumentSharedOptions{
-			FileName: "receipt_public.jpeg",
+			FileName: "receipt_public.jpg",
 			Tags:     []string{"integration", "test", "upload"},
 		},
 	})
